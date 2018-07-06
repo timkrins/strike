259fd6bdcc8bca5b1d4c1a4c2a640bd9a79f2d66
Tim Krins <timothy.krins@calligo.cloud>
2018-07-06 13:32:07 +0100

    Add test that covers truncation

--- spec/bin/strike_spec.rb
+++ spec/bin/strike_spec.rb
@@ -13,9 +13,17 @@ describe Strike, 'Use of Strike in the cli' do
     it 'should obfuscate the sql dump' do
       out = capture_io { Strike.start(params) }.join('')
 
+      # make sure original names are not retained
       out.wont_match(/Original name/)
+
       # dump_profile.rb sets this string in each name
       out.must_match(/Obfuscated name/)
+
+      # make sure device data is retained
+      out.must_match(/INSERT INTO `devices`/)
+
+      # make sure that the truncate flag works
+      out.wont_match(/INSERT INTO `audits`/)
     end
   end
 end
